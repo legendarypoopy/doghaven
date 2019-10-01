2f8468419d42ea0ff0512697de49891294fad46c
legendarypoopy <54893577+legendarypoopy@users.noreply.github.com>
2019-10-01 14:24:12 +0800

    Add files via upload

--- JShidenavbar.js
+++ JShidenavbar.js
@@ -5,7 +5,7 @@
         document.getElementById("navbar").style.top = "0";
         // document.getElementById("navbar").style.backgroundColor = "red";
       } else {
-        document.getElementById("navbar").style.top = "-100px";
+        document.getElementById("navbar").style.top = "-60px";
       }
       prevScrollpos = currentScrollPos;
     }  
