211c1d8d4e9381f1e5a5e3162f02b33f765713fe
legendarypoopy <54893577+legendarypoopy@users.noreply.github.com>
2019-10-11 18:42:06 +0800

    Add files via upload

--- JStogglemobilemenu.js
+++ JStogglemobilemenu.js
@@ -1,8 +1,34 @@
-function myFunction() {
-  var x = document.getElementById("atommenu");
-  if (x.style.display === "contents") {
-    x.style.display = "none";
+const toggle = document.querySelector('.menu_toggle');
+
+const button = document.querySelector('.nextbtn');
+
+const dogLogo = document.querySelector('#logo');
+
+const ghostMenu = document.querySelector('#mobilemenu');
+
+const navBgColor = document.querySelector('#navbar')
+
+toggle.addEventListener('click', doThis);
+
+function doThis(){
+
+  button.style.color = 'blue';
+  // dogLogo.style.backgroundColor = 'pink';
+
+  if (dogLogo.style.display === "block") {
+    dogLogo.style.display = "none";
+    navBgColor.style.backgroundColor = 'transparent';
   } else {
-    x.style.display = "contents";
+    dogLogo.style.display = "block";
+    navBgColor.style.backgroundColor = '#F3F3F3';
+
   }
+
+  if (ghostMenu.style.display === "block") {
+    ghostMenu.style.display = "none";
+  } else {
+    ghostMenu.style.display = "block";
+  }
+
+
 }
